332e5e3cdbcf53e880777ed09b7ca06ec3182b23
Chris Miller <chris@diddado.com>
2023-05-10 21:36:31 -0600

    Add rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Application related helper methods
 module ApplicationHelper
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# The main application record
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
