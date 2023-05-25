a956804c3e459e68b23626e51d98be3ce970f7ac
Chris Miller <chris@diddado.com>
2023-05-25 17:08:04 -0600

    Add home controller and appropriate routes
    The additional routes deal with things that break by using a catchall
    route.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# The main controller for the application
+class HomeController < ApplicationController
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,32 @@ Rails.application.routes.draw do
       post '/contact', to: 'contact#create'
     end
   end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web, at: '/sidekiq'
+
+  # Our catch all route is preventing ActiveStorage from working properly in development
+  get '/rails/active_storage/blobs/redirect/:signed_id/*filename' => 'active_storage/blobs/redirect#show',
+      as: :rails_service_blob_2
+  get '/rails/active_storage/blobs/proxy/:signed_id/*filename' => 'active_storage/blobs/proxy#show',
+      as: :rails_service_blob_proxy_2
+  get '/rails/active_storage/blobs/:signed_id/*filename' => 'active_storage/blobs/redirect#show'
+
+  get '/rails/active_storage/representations/redirect/:signed_blob_id/:variation_key/*filename' => 'active_storage/representations/redirect#show',
+      as: :rails_blob_representation_2
+  get '/rails/active_storage/representations/proxy/:signed_blob_id/:variation_key/*filename' => 'active_storage/representations/proxy#show',
+      as: :rails_blob_representation_proxy_2
+  get '/rails/active_storage/representations/:signed_blob_id/:variation_key/*filename' => 'active_storage/representations/redirect#show'
+
+  get  '/rails/active_storage/disk/:encoded_key/*filename' => 'active_storage/disk#show', as: :rails_disk_service_2
+  put  '/rails/active_storage/disk/:encoded_token' => 'active_storage/disk#update', as: :update_rails_disk_service_2
+  post '/rails/active_storage/direct_uploads' => 'active_storage/direct_uploads#create', as: :rails_direct_uploads_2
+
+  mount ActionCable.server => '/api/v1/cable'
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'home#index'
+
+  # catch all route for the frontend
+  match '*path', to: 'home#index', via: :all
 end
